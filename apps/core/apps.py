23eebcdcc5838fc4883137998f5cae300c2a1802
Alan Roy <post101box@gmail.com>
2026-05-12 17:50:29 +0530

    feat: initialize project boilerplate and directory structure

---
+++ apps/core/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.core'
+    label = 'core'
+
+    def ready(self):
+        from .signals import register_audit_signals
+        register_audit_signals()
---
+++ config/settings/prod.py
@@ -0,0 +1,13 @@
+# file: config/settings/prod.py
+
+from .base import *
+from decouple import config
+
+DEBUG = False
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='').split(',')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
